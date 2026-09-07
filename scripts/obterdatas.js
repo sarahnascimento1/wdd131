f37c0f2e53cffa7384ceafd811992928622f2413
sarahnascimento1 <sarahi015@outlook.com>
2026-09-07 19:34:47 -0300

    S01 Tarefa Página Inicial

---
+++ scripts/obterdatas.js
@@ -0,0 +1,6 @@
+
+const anoAtual = new Date().getFullYear();
+
+document.getElementById("anoatual").textContent = anoAtual;
+
+document.getElementById("ultimaModificacao").textContent = document.lastModified;
